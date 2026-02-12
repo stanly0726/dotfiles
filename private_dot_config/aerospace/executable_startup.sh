9f1da1b75d70b5ce56ef44ef0eb54cdb34db8e14
stanly <stanly.chang@incrte.com>
2026-02-12 16:15:07 +0800

    feat: add aerospace

---
+++ private_dot_config/aerospace/executable_startup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+open '/Applications/Microsoft Teams.app';
+/usr/local/bin/aerospace-layout-manager terminal;
+/usr/local/bin/aerospace-layout-manager browser;
+/usr/local/bin/aerospace-layout-manager note;
+/usr/local/bin/aerospace-layout-manager communication;
+aerospace workspace 1;
