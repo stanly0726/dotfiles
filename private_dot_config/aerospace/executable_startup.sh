557a7ad5d3e7958370cb7ebfec3ce60929bdbd6e
stanly <stanly.chang@incrte.com>
2026-06-22 16:30:09 +0800

    misc(aerospace): update config
    Update .config/aerospace/startup.sh
    Update .config/aerospace/layouts.json

--- private_dot_config/aerospace/executable_startup.sh
+++ private_dot_config/aerospace/executable_startup.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-open '/Applications/Microsoft Teams.app';
-/usr/local/bin/aerospace-layout-manager terminal;
-/usr/local/bin/aerospace-layout-manager browser;
-/usr/local/bin/aerospace-layout-manager note;
-/usr/local/bin/aerospace-layout-manager communication;
-aerospace workspace 1;
+/usr/local/bin/aerospace-layout-manager --debug terminal;
+/usr/local/bin/aerospace-layout-manager --debug news;
+/usr/local/bin/aerospace-layout-manager --debug browser;
+/usr/local/bin/aerospace-layout-manager --debug note;
+/usr/local/bin/aerospace-layout-manager --debug communication;
+
+osascript -e 'display notification "arranging layout complete" with title "Aerospace layout manager"'
+aerospace workspace 2;
