6696f8081cab50b6c8f38d7d98e973beeca54329
stanly <stanly.chang@incrte.com>
2026-07-14 11:29:32 +0800

    fix(aerospace): tart browser last to avoid error

--- private_dot_config/aerospace/executable_startup.sh
+++ private_dot_config/aerospace/executable_startup.sh
@@ -2,9 +2,9 @@
 
 /usr/local/bin/aerospace-layout-manager --debug terminal;
 /usr/local/bin/aerospace-layout-manager --debug news;
-/usr/local/bin/aerospace-layout-manager --debug browser;
 /usr/local/bin/aerospace-layout-manager --debug note;
 /usr/local/bin/aerospace-layout-manager --debug communication;
+/usr/local/bin/aerospace-layout-manager --debug browser;
 
 osascript -e 'display notification "arranging layout complete" with title "Aerospace layout manager"'
 aerospace workspace 2;
